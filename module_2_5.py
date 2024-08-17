88b9124f7c282a6a4c4b74cde690a7a83c4ce197
Nomottan <bocharik1994@rambler.ru>
2024-08-18 00:04:40 +0300

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -1,4 +1,4 @@
-def get_matrix(n, m, value):
+def get_matrix(n, m, value): #Решение задачи
     if n == 0 or m == 0:
         return ("Недопустимые параметры матрицы")
     matrix = []
@@ -8,9 +8,18 @@ def get_matrix(n, m, value):
             matrix[i].append(value)
     return (matrix)
 
+def matrix_print(result): #Чисто для себя чтоб разобраться, построчный вывод ответа
+    for x in range(0, len(result)):
+        row = result[x]
+        print(row)
+    print(f'\n')
+    return
+
+
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
-print(result1)
-print(result2)
-print(result3)
+
+matrix_print(result1)
+matrix_print(result2)
+matrix_print(result3)

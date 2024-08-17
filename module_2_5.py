616e5e7016f8f212f17e3e1c18379ab38559484d
Nomottan <bocharik@Rambler.ru>
2024-08-18 00:03:58 +0300

    corr

--- module_2_5.py
+++ module_2_5.py
@@ -1,15 +1,27 @@
-def get_matrix(n, m, value):
+def get_matrix(n, m, value): #Решение задачи
+    if n == 0 or m == 0:
+        return ("Недопустимые параметры матрицы")
     matrix = []
-    for i in range(n):
-        column = []
-        matrix.append(column)
-        for c in range(m):
-            column.append(value)
+    for i in range(0, n):
+        matrix.append([])
+        for c in range(0, m):
+            matrix[i].append(value)
     return (matrix)
+
+def matrix_print(result): #Чисто для себя чтоб разобраться, построчный вывод ответа
+    for x in range(0, len(result)):
+        row = result[x]
+        print(row)
+    print(f'\n')
+    return
+
 
 result1 = get_matrix(2, 2, 10)
 result2 = get_matrix(3, 5, 42)
 result3 = get_matrix(4, 2, 13)
-print(result1)
-print(result2)
-print(result3)
+
+matrix_print(result1)
+matrix_print(result2)
+matrix_print(result3)
+
+

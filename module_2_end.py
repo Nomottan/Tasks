5d47516ce25ddfaa26332300d5acd7fb07174341
Nomottan <bocharik1994@rambler.ru>
2024-08-18 19:51:58 +0300

    Add files via upload

---
+++ module_2_end.py
@@ -0,0 +1,17 @@
+import random
+
+number = random.randrange(3,21)
+print(number)
+protopassword = []
+password = []
+for i in range(1, number): #
+    for j in range(1, i):
+        if i == j:
+            break
+        if number % (i + j) == 0:
+            protopassword.append([j, i])
+protopassword.sort()
+for i in range(0, len(protopassword)): # раскрытие вложенных списков
+    password.append(protopassword[i][0])
+    password.append(protopassword[i][1])
+print(*password)

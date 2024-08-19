0a8dfdcf24b7cdd81ee93dbcfe85b7d53d012b34
Nomottan <bocharik1994@rambler.ru>
2024-08-19 20:46:43 +0300

    Update module_2_end.py

--- module_2_end.py
+++ module_2_end.py
@@ -1,20 +1,26 @@
 import random
 
 number = random.randrange(3,21)
-print(number)
 protopassword = []
 password = []
+strpassword = ''
+
+print(number)
+
 for i in range(1, number): #
     for j in range(1, i):
         if i == j:
             break
         if number % (i + j) == 0:
             protopassword.append([j, i])
+            
 protopassword.sort()
+
 for i in range(0, len(protopassword)): # раскрытие вложенных списков
     password.append(protopassword[i][0])
     password.append(protopassword[i][1])
-strpassword = ''
+    
 for i in range(0, len(password)):
     strpassword += str(password[i])
+    
 print(strpassword)

50ff02819b603071dfe159d3f9ceb6465ed8acab
Nomottan <bocharik1994@rambler.ru>
2024-08-21 22:16:47 +0300

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,23 @@
+def проверка_почты (recipient, sender):
+    for проверка in (recipient, sender):
+        if '@' not in проверка:
+                return False
+        for искомое in (".com", ".ru", ".net"):
+            if искомое in проверка:
+                return True
+    return False
+def send_email (message, recipient, sender = "university.help@gmail.com"):
+    if recipient == sender:
+        print(f'Нельзя отправить письмо самому себе!')
+    elif not проверка_почты(recipient, sender):
+        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+    elif sender == "university.help@gmail.com":
+        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
+    else:
+        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
+    return
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')

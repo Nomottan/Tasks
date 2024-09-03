a6930b80f344a100ef9b12bcadc96c843de54022
Nomottan <bocharik1994@rambler.ru>
2024-09-03 12:02:42 +0300

    Add files via upload

---
+++ module_4_1/module_4_1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as fdif
+from true_math import divide as tdif
+
+
+result1 = fdif(69, 3)
+result2 = fdif(3, 0)
+result3 = tdif(49, 7)
+result4 = tdif(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)

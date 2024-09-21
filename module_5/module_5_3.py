88ae2c1581f7a34dee68f0f734df30c87346acf9
Nomottan <bocharik1994@rambler.ru>
2024-09-21 12:28:00 +0300

    Add files via upload

---
+++ module_5/module_5_3.py
@@ -0,0 +1,70 @@
+class House:
+    def __init__(self, name, num_of_floors):
+        self.name = name
+        self.num_of_floors = int(num_of_floors)
+
+    def __len__(self):
+        return self.num_of_floors
+
+    def __str__(self):
+        return self.name
+
+    def __eq__(self, other):
+        return self.num_of_floors == other.num_of_floors
+
+    def __lt__(self, other):
+        return self.num_of_floors < other.num_of_floors
+
+    def __le__(self, other):
+        return self.num_of_floors <= other.num_of_floors
+
+    def __gt__(self, other):
+        return self.num_of_floors > other.num_of_floors
+
+    def __ge__(self, other):
+        return self.num_of_floors >= other.num_of_floors
+
+    def __ne__(self, other):
+        return self.num_of_floors != other.num_of_floors
+
+    def __add__(self, value):
+        self.num_of_floors += value
+        return self
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, value):
+        return self.__add__(value)
+
+    def go_to(self, new_floor):
+        num_of_floors = int(self.num_of_floors)
+        if new_floor > num_of_floors:
+            print (f"Этаж {new_floor} в здании {self.name} не существует")
+            return
+        for f in range(1, new_floor+1):
+            print(f)
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1.name, h1.num_of_floors)
+print(h2.name, h2.num_of_floors)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1.name, h1.num_of_floors)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1.name, h1.num_of_floors)
+
+h2 = 10 + h2 # __radd__
+print(h2.name, h2.num_of_floors)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__

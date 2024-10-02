2a817f068dbc359874c47d81736b9d349cac4749
Nomottan <bocharik1994@rambler.ru>
2024-10-02 20:28:49 +0300

    Update module_5_hard.py

--- module_5/module_5_hard.py
+++ module_5/module_5_hard.py
@@ -9,18 +9,19 @@ class UrTube:
     def log_in(self, nickname, password):
         for nick in self.users:
             if nickname in nick:
-                if nickname.password == hash(password):
+                if nick == password:
                     self.current_user = nickname
                 else:
                     print(f"Пароль не верный")
 
     def register(self, nickname, password, age):
-        if nickname not in self.users:
-            nickname = User(nickname, password, age)
-            self.users.append(User)
-            self.current_user = nickname
-        else:
-            print(f"Пользователь {nickname} уже зарегестрирован")
+        for user in self.users:
+            if nickname in user:
+                print(f"Пользователь {nickname} уже зарегестрирован")
+                return
+        nickname = User(nickname, password, age)
+        self.users.append(nickname)
+        self.current_user = nickname
 
     def log_out(self):
         self.current_user = None
@@ -40,9 +41,19 @@ class UrTube:
         return my_lyst
 
     def watch_video(self, title):
-        return
-
-
+        if self.current_user != None:
+            for Video in self.videos:
+                if Video == title:
+                    if Video.adult_mode and self.current_user <= 18:
+                            print(f'Вам нет 18 лет, пожалуйста покиньте страницу')
+                            break
+                    for sec in range(0, len(Video)):
+                        print(sec)
+                        time.sleep(1)
+                    print('Конец видео')
+                    break
+        else:
+            print(f'Войдите в аккаунт, чтобы смотреть видео')
 
 
 
@@ -62,7 +73,7 @@ class Video:
 
     def __contains__(self, title):
         return title.lower() in self.title.lower()
-    
+
     def __eq__(self, other):
         return self.title == other
 
@@ -78,12 +89,13 @@ class User:
         return self.nickname
 
     def __eq__(self, other):
-        return self.password == other
+        return self.password == hash(other)
+
     def __le__(self, other):
-        return self.age < other
+        return self.age <= other
 
     def __contains__(self, nickname):
-        return nickname in self.nickname
+        return nickname == self.nickname
 
 
 ur = UrTube()
@@ -92,10 +104,21 @@ v2 = Video('Для чего девушкам парень программист
 
 # Добавление видео
 ur.add(v1, v2)
+
+# Проверка поиска
 print(ur.get_videos('лучший'))
 print(ur.get_videos('ПРОГ'))
 
+# Проверка на вход пользователя и возрастное ограничение
+ur.watch_video('Для чего девушкам парень программист?')
 ur.register('vasya_pupkin', 'lolkekcheburek', 13)
+ur.watch_video('Для чего девушкам парень программист?')
 ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
-ur.log_in('vasya_pupkin', 'lolkekcheburek')
+ur.watch_video('Для чего девушкам парень программист?')
+
+# Проверка входа в другой аккаунт
+ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
 print(ur.current_user)
+
+# Попытка воспроизведения несуществующего видео
+ur.watch_video('Лучший язык программирования 2024 года!')

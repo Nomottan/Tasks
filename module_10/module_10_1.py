6b8a4f373db4a77220d7cf8431f135db7a18511f
Nomottan <bocharik1994@rambler.ru>
2024-10-28 12:49:21 +0300

    Update module_10_1.py

--- module_10/module_10_1.py
+++ module_10/module_10_1.py
@@ -1,4 +1,4 @@
-from time import sleep
+import time
 import threading
 
 
@@ -6,26 +6,31 @@ def write_words(word_count, file_name):
     with open(file_name, "w", encoding='utf-8') as fn:
         for number in range(word_count):
             fn.write(f"Какое-то слово №{number}\n")
-            sleep(0.1)
+            time.sleep(0.1)
     return print(f"Завершилась запись в файл {file_name}")
 
 
+startes_at = time.time()
 write_words(10, "example1.txt")
 write_words(30, "example2.txt")
 write_words(200, "example3.txt")
 write_words(100, "example4.txt")
+ended_at = time.time()
+print(f'Работа потоков {round(ended_at - startes_at, 4)}')
 
-thread = threading.Thread(target=write_words(10, "example5.txt"))
-thread.start()
-thread.join()
-thread_0 = threading.Thread(target=write_words(30, "example6.txt"))
-thread_0.start()
-thread_0.join()
-print(threading.current_thread().is_alive())
-thread_1 = threading.Thread(target=write_words(200, "example7.txt"))
-thread_1.start()
-thread_1.join()
-thread_2 = threading.Thread(target=write_words(100, "example8.txt"))
-thread_2.start()
-thread_2.join()
+startes_at = time.time()
+threads = [
+    threading.Thread(target=write_words, args=(10, "example5.txt", )),
+    threading.Thread(target=write_words, args=(30, "example6.txt", )),
+    threading.Thread(target=write_words, args=(300, "example7.txt", )),
+    threading.Thread(target=write_words, args=(100, "example8.txt", ))
+    ]
 
+for thread in threads:
+    thread.start()
+
+for thread in threads:
+    thread.join()
+
+ended_at = time.time()
+print(f'Работа потоков {round(ended_at - startes_at, 4)}')

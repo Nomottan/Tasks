77a6af14f9e78a19dc5f0e6b31a0ae5bba382aca
Nomottan <bocharik1994@rambler.ru>
2024-11-05 14:22:32 +0300

    Add files via upload

---
+++ module_10/module_10_3.py
@@ -0,0 +1,46 @@
+import threading
+from random import randint
+from time import sleep
+
+
+class Bank:
+    def __init__(self):
+        self.balance =0
+        self.lock = threading.Lock()
+
+    def deposit(self):
+        count = 0
+        while count != 100:
+            if self.balance > 500:
+                self.lock.release()
+            dep = randint(50, 500)
+            self.balance += dep
+            print(f"Пополнение: {dep}.Баланс:{self.balance}")
+            count += 1
+            sleep(0.001)
+
+    def take(self):
+        count = 0
+        while count != 100:
+            tak = randint(50, 500)
+            if tak <= self.balance:
+                self.balance -= tak
+                print(f"Снятие {tak}.Баланс:{self.balance}")
+            else:
+                self.lock.acquire()
+            sleep(0.001)
+            count += 1
+
+
+
+bk = Bank()
+
+th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
+
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+
+print(f'Итоговый баланс: {bk.balance}')

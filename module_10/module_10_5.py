228514e5ab5dc674e5e4af0817933bbb794b8977
Nomottan <bocharik1994@rambler.ru>
2024-11-07 19:38:04 +0300

    Update module_10_5.py

--- module_10/module_10_5.py
+++ module_10/module_10_5.py
@@ -1,11 +1,14 @@
 from time import time
-from multiprocessing import Process
+import multiprocessing
+
 
 def read_info(name):
-    with open(name, 'r') as file:
-        all_data = []
-        for line in file:
-            all_data.append(line)
+    all_data = []
+    with open(name, 'r', encoding='utf-8') as file:
+        data = file.readline()
+        while data:
+            data = file.readline()
+            all_data.append(data[0:-1])
 
 
 filenames = [f'./file {number}.txt' for number in range(1, 5)]
@@ -17,9 +20,9 @@ print(f"{round(end-start, 6)} (линейный)")
 start = time()
 
 if __name__ == '__main__':
-    for file in filenames:
-        proc = Process(target=read_info, args=(file, ))
-        proc.start()
+    with multiprocessing.Pool(processes=4) as pool:
+        pool.map(read_info, filenames)
+
 
 end = time()
 print(f"{round(end - start, 6)} (многопроцессный)")

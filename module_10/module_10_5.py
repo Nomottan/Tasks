e45450c970c194ec7531b5863e45b4b34d83d0f7
Nomottan <bocharik1994@rambler.ru>
2024-11-07 13:44:41 +0300

    Add files via upload

---
+++ module_10/module_10_5.py
@@ -0,0 +1,26 @@
+from time import time
+from multiprocessing import Process
+
+def read_info(name):
+    with open(name, 'r') as file:
+        all_data = []
+        for line in file:
+            all_data.append(line)
+
+
+filenames = [f'./file {number}.txt' for number in range(1, 5)]
+
+start = time()
+list(map(read_info, filenames))
+end = time()
+print(f"{round(end-start, 6)} (линейный)")
+start = time()
+
+if __name__ == '__main__':
+    filenames = [f'./file {number}.txt' for number in range(1, 5)]
+    for file in filenames:
+        proc = Process(target=read_info, args=(file, ))
+        proc.start()
+
+end = time()
+print(f"{round(end - start, 6)} (многопроцессный)")

25a898ed2729ac365d849759201dcf8f813c1dd4
Nomottan <bocharik1994@rambler.ru>
2024-12-09 18:17:44 +0300

    Add files via upload

--- module_12/module_12_2.py
+++ module_12/module_12_2.py
@@ -18,19 +18,24 @@ class TournamentTest(unittest.TestCase):
             print(result)
 
     def TourTest(self):
-        tour = testRunner.Tournament(90, (self.runner1, self.runner3))   # создание объекта Tournament
-        tourresult = tour.start()                                                           # вызов метода "stаrt" в объекте Tournament
-        self.all_results.copy(tourresult)                                                   # сохранение результатов в список
-        self.assertTrue(tourresult[2] == "Ник")                                     # проверка результата по ключу в словаре
-        tour2 = testRunner.Tournament(90, (self.runner2, self.runner3))
-        tourresult1 = tour2.start()
-        self.all_results.copy(tourresult1)
-        self.assertTrue(tourresult1[2] == "Ник")
-        tour3 = testRunner.Tournament(90, (self.runner1, self.runner2, self.runner3))
-        tourresult2 = tour3.start()
-        self.all_results.copy(tourresult2)
-        self.assertTrue(tourresult2[3] == "Ник")
+        tour = testRunner.Tournament(90, (self.runner1, self.runner3))
+        tour_result = tour.start()
+        new_keys = {len(self.__class__.all_result) + k: v for k, v in tour_result.items()}
+        self.__class__.all_results.update(new_keys)
+        self.assertTrue(tour_result[max(tour_result.keys())] == self.runner3)
+        # tour2 = testRunner.Tournament(90, (self.runner2, self.runner3))
+        # tour2_result = tour2.start()
+        # new_keys = {len(self.all_result) + k: v for k, v in tour2_result.items()}
+        # self.all_results.update(new_keys)
+        # self.assertTrue(tour2_result[max(tour2_result.keys())])
+        # tour3 = testRunner.Tournament(90, (self.runner1, self.runner2, self.runner3))
+        # tour3_result = tour3.start()
+        # new_keys = {len(self.all_result) + k: v for k, v in tour3_result.items()}
+        # self.all_results.update(new_keys)
+        # self.assertTrue(tour_result[max(tour3_result.keys())])
 
 
 if __name__ == "__main__":
     unittest.main()
+
+

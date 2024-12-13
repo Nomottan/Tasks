ac4f08b52b9e1ddf56c58a3addce73cf5579fd64
Nomottan <bocharik1994@rambler.ru>
2024-12-13 16:26:23 +0300

    Update tests_12_4.py

--- module_12/tests_12_4.py
+++ module_12/tests_12_4.py
@@ -5,31 +5,30 @@ import logging
 logging.basicConfig(level=logging.INFO, filemode="w", filename="runner_tests.log",
                     encoding='UTF-8', format="%(levelname)s | %(message)s")
 
+
 class RunnerTest(unittest.TestCase):
 
     def test_walk(self):
         try:
             testwalk = Runner("test", -5)
+            for step in range(0, 10):
+                testwalk.walk()
+            self.assertEqual(testwalk.distance, 50)
+            logging.info('"test_walk" выполнен успешно')
         except ValueError as verr:
             logging.warning('ValueError', exc_info=True)
             return
+
+    def test_run(self):
+        try:
+            testrun = Runner(15, 15)
+        except TypeError as terr:
+            logging.warning('TypeError', exc_info=True)
+            return
         for step in range(0, 10):
-            testwalk.walk()
-        self.assertEqual(testwalk.distance, 50)
-        logging.info('"test_walk" выполнен успешно')
-
-
-    try:
-        def test_run(self):
-            testrun = Runner("test")
-            for step in range(0, 10):
-                testrun.run()
-            self.assertEqual(testrun.distance, 100)
-            logging.info('"test_run" выполнен успешно')
-    except TypeError as terr:
-        logging.warning('TypeError', exc_info=True)
-
+            testrun.run()
+        self.assertEqual(testrun.distance, 100)
+        logging.info('"test_run" выполнен успешно')
 
 
 runtest = RunnerTest()
-runtest.test_run()

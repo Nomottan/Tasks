6c926b2db323510621a20af45be80344016e517d
Nomottan <bocharik1994@rambler.ru>
2024-12-13 15:34:29 +0300

    Update module_12_1.py

--- module_12/module_12_1.py
+++ module_12/module_12_1.py
@@ -3,19 +3,23 @@ import unittest
 
 
 class RunnerTest(unittest.TestCase):
+    is_frozen = False
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_walk(self):
         testwalk = Runner("test")
         for step in range(0, 10):
             testwalk.walk()
         self.assertEqual(testwalk.distance, 50)
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_run(self):
         testrun = Runner("test")
         for step in range(0, 10):
             testrun.run()
         self.assertEqual(testrun.distance, 100)
 
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
     def test_challenge(self):
         testrun = Runner("test")
         testwalk = Runner("test")
@@ -28,4 +32,3 @@ class RunnerTest(unittest.TestCase):
 
 runtest = RunnerTest()
 runtest.test_run()
-

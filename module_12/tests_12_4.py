2994357096b00210c5044f6f271f091e71be720a
Nomottan <bocharik1994@rambler.ru>
2024-12-13 16:19:31 +0300

    Update tests_12_4.py

--- module_12/tests_12_4.py
+++ module_12/tests_12_4.py
@@ -2,19 +2,22 @@ import unittest
 from testRunnerext import Runner
 import logging
 
+logging.basicConfig(level=logging.INFO, filemode="w", filename="runner_tests.log",
+                    encoding='UTF-8', format="%(levelname)s | %(message)s")
 
 class RunnerTest(unittest.TestCase):
-    is_frozen = False
 
     def test_walk(self):
         try:
             testwalk = Runner("test", -5)
-            for step in range(0, 10):
-                testwalk.walk()
-            self.assertEqual(testwalk.distance, 50)
-            logging.info('"test_walk" выполнен успешно')
         except ValueError as verr:
             logging.warning('ValueError', exc_info=True)
+            return
+        for step in range(0, 10):
+            testwalk.walk()
+        self.assertEqual(testwalk.distance, 50)
+        logging.info('"test_walk" выполнен успешно')
+
 
     try:
         def test_run(self):
@@ -27,8 +30,6 @@ class RunnerTest(unittest.TestCase):
         logging.warning('TypeError', exc_info=True)
 
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, filemode="w", filename="runner_tests.log",
-                        encoding='UTF-8', format="%(levelname)s | %(message)s")
-    runtest = RunnerTest()
-    runtest.test_run()
+
+runtest = RunnerTest()
+runtest.test_run()

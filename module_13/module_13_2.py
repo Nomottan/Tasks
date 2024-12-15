ee7bc2f4d3c1178f989ff5eac7a6943bf6d33e4d
Nomottan <bocharik1994@rambler.ru>
2024-12-15 07:35:40 +0300

    Add files via upload

---
+++ module_13/module_13_2.py
@@ -0,0 +1,20 @@
+import asyncio
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+
+api = "7726405232:AAGizj4ox2RWHbJCid76XWZQlg-SkbIJxcg"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print(f"Привет! Я бот помогающий твоему здоровью.")
+
+
+@dp.message_handler()
+async def all_messages(message):
+    print(f"Введите команду /start, чтобы начать общение.'.")
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)

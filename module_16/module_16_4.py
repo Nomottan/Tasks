dfb48d796bdebf510b4cfd7c6caf9482ca0699e3
Nomottan <bocharik1994@rambler.ru>
2025-01-22 15:33:25 +0300

    Update module_16_4.py

--- module_16/module_16_4.py
+++ module_16/module_16_4.py
@@ -18,10 +18,23 @@ async def get_all_users() -> List[User]:
 
 
 @app.post("/user/{username}/{age}")
-async def add_user(username: str, age: str) -> str:
-    curr_us = User()
-    curr_us.id = len(users)
-    curr_us.username = str(username)
-    curr_us.age = int(age)
-    users.append(curr_us)
-    return f"{curr_us} is registered"
+
+@app.post('/user/{username}/{age}')
+async def add_user(username: str, age: str) -> User:
+    user_id = max(users, key=lambda x: int(x.id)).id + 1 if users else 1
+    user = User(id=user_id, username=username, age=age)
+    users.append(user)
+    return user
+
+
+# @app.put("/user/{user_id}/{username}/{age}")
+# async def update_user(user_id: str, username: str, age: int) -> str:
+#     user = list(filter(lambda users: user.id == user_id))
+#     users[user_id] = f'Имя: {username}, возраст: {age}'
+#     return f"User {user_id} is updated"
+#
+#
+# @app.delete("/user/{user_id}")
+# async def delete_user(user_id: str) -> str:
+#     users.pop(user_id)
+#     return f"User {user_id} has been deleted"

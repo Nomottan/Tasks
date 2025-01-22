3688827bb193d6aded7da099f28470538d02d615
Nomottan <bocharik1994@rambler.ru>
2025-01-22 17:38:29 +0300

    Update module_16_4.py

--- module_16/module_16_4.py
+++ module_16/module_16_4.py
@@ -18,7 +18,7 @@ async def get_all_users() -> List[User]:
 
 
 @app.post('/user/{username}/{age}')
-async def add_user(username: str, age: str) -> User:
+async def add_user(username: str, age: int) -> User:
     user_id = max(users, key=lambda x: int(x.id)).id + 1 if users else 1
     user = User(id=user_id, username=username, age=age)
     users.append(user)
@@ -26,23 +26,24 @@ async def add_user(username: str, age: str) -> User:
 
 
 @app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_id: str, username: str, age: int) -> str:
+async def update_user(user_id: int, username: str, age: int) -> str:
     try:
         user = [us for us in users if us.id == user_id]
+        user = user[0]
         user.username = username
         user.age = age
         return f"User {user_id} is updated"
     except IndexError:
-        raise HTTPException(status_code = 404, detail="User was not found")
+        raise HTTPException(status_code=404, detail="User was not found")
 
 
 
 @app.delete("/user/{user_id}")
-async def delete_user(user_id: str) -> str:
+async def delete_user(user_id: int) -> str:
     try:
         user = [us for us in users if us.id == user_id]
+        user = user[0]
         users.pop(user)
         return f"User {user_id} has been deleted"
     except IndexError:
         raise HTTPException(status_code=404, detail="User was not found")
-        
